521d26faba52d85eb13fdfdca2330d1f5c6e3ebd
nicole <nicolef@post.bgu.ac.il>
2020-06-08 16:02:49 +0300

    personal recipe nicole

---
+++ codes/sql scripts/personal recipes nicole.sql
@@ -0,0 +1,65 @@
+--------------------------VEGAN CREAMED POTATO-------------------
+
+Insert into personalRecipes values (34, '9b6c41d7-009e-4994-801e-18a8bf440951', 'Vegan creamed potato', 'https://veganinthefreezer.com/wp-content/uploads/2018/09/Herbed-Scalloped-Potatoes-feature.jpg', '90', '1', '1', '0', '10');
+
+Insert into personalIngredients values (34, 'potatoes', '8', '');
+Insert into personalIngredients values (34, 'coconut cream', '1', 'package');
+Insert into personalIngredients values (34, 'onion', '1', '');
+Insert into personalIngredients values (34, 'salt', '1', 'teaspoon');
+Insert into personalIngredients values (34, 'Black spoon', '1', 'teaspoon');
+Insert into personalIngredients values (34, 'nutmeg', '1/2', 'teaspoon');
+Insert into personalIngredients values (34, 'cloves of garlic', '6', '');
+Insert into personalIngredients values (34, 'olive oil', '1', 'tablespoon ');
+Insert into personalIngredients values (34, 'oil', '1', 'teaspoon ');
+Insert into personalIngredients values (34, 'csweet paprika', '2', 'teaspoons ');
+Insert into personalIngredients values (34, 'breadcrumbs', '3', 'tablespoons');
+Insert into personalIngredients values (34, 'hot water', '1', 'cup');
+
+
+Insert into personalInstructions values (34, 1, '');
+Insert into personalInstructions values (34, 2, '');
+Insert into personalInstructions values (34, 3, '');
+Insert into personalInstructions values (34, 4, '');
+Insert into personalInstructions values (34, 5, '');
+
+
+--------------------------WHITE RICE-------------------
+
+Insert into personalRecipes values (35, '9b6c41d7-009e-4994-801e-18a8bf440951', 'White Rice', 'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18195812/fc31tc016-03-thumb16x9.jpg', '30', '1', '1', '0', '8');
+
+Insert into personalIngredients values (35, 'white rice', '1.5', 'cups');
+Insert into personalIngredients values (35, 'onion', '1', '');
+Insert into personalIngredients values (35, 'salt', '1.5', 'teaspoon');
+Insert into personalIngredients values (35, 'hot water', '3', 'cups ');
+Insert into personalIngredients values (35, 'olive oil', '2', 'teaspoons  ');
+
+Insert into personalInstructions values (35, 1, '');
+Insert into personalInstructions values (35, 2, '');
+Insert into personalInstructions values (35, 3, '');
+Insert into personalInstructions values (35, 4, '');
+Insert into personalInstructions values (35, 5, '');
+
+
+---------------------------CHOCOLATE CAKE-----------------------------------
+
+Insert into personalRecipes values (36, '9b6c41d7-009e-4994-801e-18a8bf440951', 'Chocolate Cake for Lazy Students', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSyl4MZQl6tYlZa7FnTuZ_FGoTe8gQaz05htOKDPQjJSdH8K88l&usqp=CAU', '40', '0', '1', '0', '12');
+
+Insert into personalIngredients values (36, 'flour', '1', 'cup');
+Insert into personalIngredients values (36, 'chocolates', '1', 'cup');
+Insert into personalIngredients values (36, 'oil', '1', 'cup');
+Insert into personalIngredients values (36, 'white sugar', '1', 'cup');
+Insert into personalIngredients values (36, 'eggs', '5', '');
+Insert into personalIngredients values (36, 'baking powder', '1', 'teaspoon');
+Insert into personalIngredients values (36, 'sweet cream', '1', 'container');
+Insert into personalIngredients values (36, 'olive oil', '1', 'tablespoon ');
+Insert into personalIngredients values (36, 'oil', '1', 'teaspoon ');
+Insert into personalIngredients values (36, 'csweet paprika', '2', 'teaspoons ');
+Insert into personalIngredients values (36, 'milk chocolate ', '1', 'table');
+Insert into personalIngredients values (36, 'dark chocolate ', '1', 'table');
+
+
+Insert into personalInstructions values (36, 1, '');
+Insert into personalInstructions values (36, 2, '');
+Insert into personalInstructions values (36, 3, '');
+Insert into personalInstructions values (36, 4, '');
+Insert into personalInstructions values (36, 5, '');
