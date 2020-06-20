f348da9d7fba03d6d24c020a80bbdcde1ce296f9
adiavinun <49281852+adiavinun@users.noreply.github.com>
2020-06-20 16:55:04 +0300

    .

--- codes/main.js
+++ codes/main.js
@@ -5,7 +5,7 @@ var path = require("path");
 var logger = require("morgan");
 const session = require("client-sessions");
 const DButils = require("./routes/utils/DButils");
-var cors = require("cors");
+const cors = require("cors");
 
 const corsConfig = {
   origin: true,
