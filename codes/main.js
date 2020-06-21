673c6d807b494e347dbbfc516c507a6ab7827efe
adiavinun <49281852+adiavinun@users.noreply.github.com>
2020-06-21 16:54:25 +0300

    changed cookie session duration

--- codes/main.js
+++ codes/main.js
@@ -14,7 +14,7 @@ app.use(
   session({
     cookieName: "session", // the cookie key name
     secret: "secret", // the encryption key
-    duration: 20 * 60 * 1000, // expired after 20 sec
+    duration: 600 * 60 * 1000, // expired after 600 sec
     activeDuration: 0 // if expiresIn < activeDuration,
     //the session will be extended by activeDuration milliseconds
   })
