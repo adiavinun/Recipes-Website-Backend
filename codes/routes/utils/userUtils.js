9ec73c06a2458b1c1f17ee14c887e52c555146dc
adiavinun <49281852+adiavinun@users.noreply.github.com>
2020-07-10 17:20:50 +0300

    .

--- codes/routes/utils/userUtils.js
+++ codes/routes/utils/userUtils.js
@@ -78,7 +78,7 @@ async function getMyPersonalRecipeFull(user_id, recipe_id) {
     }
     let personalIngredients = (
       await DButils.execQuery(
-        `SELECT ingredient, nameAndAmount FROM dbo.personalIngredients WHERE id= '${recipe_id}' ORDER BY number ASC`
+        `SELECT nameAndAmount FROM dbo.personalIngredients WHERE id= '${recipe_id}' ORDER BY number ASC`
       )
     );
     let personalInstructions = (
